7dc3012d2bd1d02db8c84de576451fd2822533fd
sobhan <sobhanb7b7@protonmail.com>
2026-07-06 02:08:32 +0330

    fix reservation logic based on prof instructure

--- models/Reservation.java
+++ models/Reservation.java
@@ -1,25 +1,30 @@
-import java.time.LocalDate;
+import java.util.List;
 
-
-import enums.ReservationStatus;
 import interfaces.Billable;
+import interfaces.Filterable;
+import interfaces.Searchable;
 
-public class Reservation implements Billable {
-
-    private String reservationId;
-    private Guest guest;
-    private Room room;
-
-
-    private ReservationStatus status;
-
+public class Reservation implements
+        Billable,
+        Searchable<Reservation>,
+        Filterable<Reservation> {
 
     @Override
     public double calculateTotal() {
-        return 0; 
+        return 0;
     }
 
     @Override
     public void applyDiscount(double percent) {
+    }
+
+    @Override
+    public List<Reservation> search(String query) {
+        return null;
+    }
+
+    @Override
+    public List<Reservation> filter(java.util.function.Predicate<Reservation> predicate) {
+        return null;
     }
 }
