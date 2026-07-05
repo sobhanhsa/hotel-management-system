746bed15afe2ac5a0f56ce9ad46a6ab07edaf4db
sobhan <sobhanb7b7@protonmail.com>
2026-07-06 02:02:30 +0330

    fix reservation logic

--- models/Reservation.java
+++ models/Reservation.java
@@ -1,11 +1,8 @@
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+
 
 import enums.ReservationStatus;
 import interfaces.Billable;
-import interfaces.Notifiable;
-import interfaces.RoomObserver;
 
 public class Reservation implements Billable {
 
@@ -13,15 +10,8 @@ public class Reservation implements Billable {
     private Guest guest;
     private Room room;
 
-    private LocalDate checkInDate;
-    private LocalDate checkOutDate;
 
     private ReservationStatus status;
-
-    private int numberOfGuests;
-
-    private List<Service> services = new ArrayList<>();
-    private List<Payment> payments = new ArrayList<>();
 
 
     @Override
