eeae39af19f5f7d4137b5d743ec034d459c8b885
sobhan <sobhanb7b7@protonmail.com>
2026-07-06 02:35:38 +0330

    fix type

--- interfaces/Filterable.java
+++
@@ -1,8 +0,0 @@
-package interfaces;
-
-import java.util.List;
-import java.util.function.Predicate;
-
-public interface Filterable<T> {
-    List<T> filter(Predicate<T> predicate);
-}
--- interfaces/Searchable.java
+++ interfaces/Searchable.java
@@ -1,7 +1,9 @@
 package interfaces;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public interface Searchable<T> {
     List<T> search(String query);
+    List<T> filter(Predicate<T> predicate);
 }
