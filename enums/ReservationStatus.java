027bfe168f664ef2b91b421bfb88c8b1f381a662
sobhan <sobhanb7b7@protonmail.com>
2026-07-06 00:48:47 +0330

    implement needed types

---
+++ enums/ReservationStatus.java
@@ -0,0 +1,9 @@
+package enums;
+
+public enum ReservationStatus {
+    PENDING,
+    CONFIRMED,
+    ACTIVE,
+    COMPLETED,
+    CANCELLED
+}
---
+++ enums/Season.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum Season {
+    PEAK,     // تابستان → 1.5
+    NORMAL,   // بهار/پاییز → 1.2
+    OFF       // زمستان → 0.8
+}
