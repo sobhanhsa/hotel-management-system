aefd5d9be7bb77d31af39c66e783f13ff2dc6fef
sobhan <sobhanb7b7@protonmail.com>
2026-07-07 11:28:26 +0330

    add dicount rate into membership enum

--- enums/MembershipLevel.java
+++ enums/MembershipLevel.java
@@ -1,8 +1,19 @@
 package enums;
 
 public enum MembershipLevel {
-    BRONZE,
-    SILVER,
-    GOLD,
-    PLATINUM
+
+    BRONZE(0),
+    SILVER(0.05),
+    GOLD(0.10),
+    PLATINUM(0.15);
+
+    private final double discountRate;
+
+    MembershipLevel(double discountRate) {
+        this.discountRate = discountRate;
+    }
+
+    public double getDiscountRate() {
+        return discountRate;
+    }
 }
