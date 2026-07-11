c9a6faff4c17c3ffc78820888c2cab65a905fd44
sobhan <sobhanb7b7@protonmail.com>
2026-07-11 11:41:24 +0330

    move and redefine report

---
+++ models/Report.java
@@ -0,0 +1,27 @@
+package managers;
+
+import java.time.LocalDateTime;
+
+import interfaces.Exportable;
+
+public abstract class Report implements Exportable {
+
+    protected String title;
+    protected LocalDateTime generatedAt;
+
+    public Report(String title) {
+        this.title = title;
+        this.generatedAt = LocalDateTime.now();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalDateTime getGeneratedAt() {
+        return generatedAt;
+    }
+
+    @Override
+    public abstract String exportToText();
+}
