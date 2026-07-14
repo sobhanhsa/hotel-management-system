f09f08e7efc04ae407b1800e757e430f296197e5
sobhan <sobhanb7b7@protonmail.com>
2026-07-14 10:35:01 +0330

    Add room management operations

--- managers/RoomManager.java
+++ managers/RoomManager.java
@@ -4,7 +4,10 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import enums.RoomStatus;
 import enums.RoomType;
+import exceptions.DuplicateRoomException;
+import exceptions.RoomNotFoundException;
 import models.Room;
 
 public class RoomManager {
@@ -29,5 +32,51 @@ public class RoomManager {
                     )
                 )
                 .toList();
+    }
+
+    public Room findRoom(String roomNumber) {
+        for (Room room : rooms) {
+            if (room.getRoomNumber().equals(roomNumber)) {
+                return room;
+            }
+        }
+
+        return null;
+    }
+
+    public boolean removeRoom(String roomNumber) throws RoomNotFoundException {
+        Room room = findRoom(roomNumber);
+
+        if (room == null) {
+            throw new RoomNotFoundException("Room " + roomNumber + " not found");
+        }
+
+        rooms.remove(room);
+        return true;
+    }
+
+    public void addRoom(Room room) throws DuplicateRoomException {
+
+        if (findRoom(room.getRoomNumber()) != null) {
+            throw new DuplicateRoomException(
+                    "Room " + room.getRoomNumber() + " already exists"
+            );
+        }
+
+        rooms.add(room);
+    }
+
+    public void changeRoomStatus(String roomNumber, RoomStatus newStatus)
+        throws RoomNotFoundException {
+
+        Room room = findRoom(roomNumber);
+
+        if (room == null) {
+            throw new RoomNotFoundException(
+                    "Room with number " + roomNumber + " does not exist"
+            );
+        }
+
+        room.setStatus(newStatus);
     }
 }
