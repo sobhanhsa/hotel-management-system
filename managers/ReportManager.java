975bf4fba0c34bc5db9908632ab8187adfd0b093
Sobhan <sobhanb7b7@protonmail.com>
2026-07-14 19:49:32 +0330

    refactor ReportManager to use managers

--- managers/ReportManager.java
+++ managers/ReportManager.java
@@ -2,7 +2,6 @@ package managers;
 
 import java.time.LocalDate;
 import java.time.YearMonth;
-import java.util.*;
 
 import models.DebtorReport;
 import models.Guest;
@@ -10,61 +9,61 @@ import models.GuestHistoryReport;
 import models.IncomeReport;
 import models.OccupancyReport;
 import models.PopularRoomReport;
-import models.Reservation;
-import models.Room;
 import models.RoomStatusReport;
 
 public class ReportManager {
 
-    private List<Room> rooms;
-    private List<Reservation> reservations;
+    private final RoomManager roomManager;
+    private final ReservationEngine reservationEngine;
 
-    public ReportManager(List<Room> rooms,
-        List<Reservation> reservations,
-        List<Guest> guests) {
-
-        this.rooms = rooms;
-        this.reservations = reservations;
+    public ReportManager(
+        RoomManager roomManager,
+        ReservationEngine reservationEngine
+    ) {
+        this.roomManager = roomManager;
+        this.reservationEngine = reservationEngine;
     }
 
 
     // R01 - Room Status Report
     public String roomStatusReport() {
-        return new RoomStatusReport(rooms).exportToText();
+        return new RoomStatusReport(roomManager.getRooms()).exportToText();
     }
 
 
 
     // R02 - Daily Occupancy Report
     public String occupancyReport(LocalDate date) {
-
-        return new OccupancyReport(rooms, reservations, date).exportToText();
+        return new OccupancyReport(
+                roomManager.getRooms(),
+                reservationEngine.getReservations(),
+                date
+        ).exportToText();
     }
-
 
 
     // R03 - Monthly Income Report
     public String monthlyIncomeReport(YearMonth month) {
-        return new IncomeReport(reservations, month).exportToText();
+        return new IncomeReport(
+                reservationEngine.getReservations(),
+                month
+        ).exportToText();
     }
-
-
 
 
     // R04 - Most Popular Rooms
     public String popularRoomsReport() {
-
-        return new PopularRoomReport(reservations).exportToText();
+        return new PopularRoomReport(
+                reservationEngine.getReservations()
+        ).exportToText();
     }
-
-
-
 
 
     // R05 - Debtors Report
     public String debtorsReport() {
-
-        return new DebtorReport(reservations).exportToText();
+        return new DebtorReport(
+                reservationEngine.getReservations()
+        ).exportToText();
     }
 
 
